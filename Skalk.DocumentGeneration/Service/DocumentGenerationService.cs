3ad077357ce655d66ae190baf8ff1acba0fe3a7a
pospyra <pos.pyra@yandex.ru>
2023-12-21 11:34:52 +0300

    deleted COM Microsoft.Office.Interop.Word

--- Skalk.DocumentGeneration/Service/DocumentGenerationService.cs
+++ Skalk.DocumentGeneration/Service/DocumentGenerationService.cs
@@ -1,9 +1,10 @@
-﻿using System.Runtime.InteropServices;
-using Microsoft.Office.Interop.Word;
+﻿using ConvertApiDotNet;
 using Skalk.Common.DTO.Order;
 using Skalk.DocumentGeneration.Interfaces;
 using Skalk.DocumentGeneration.Service.Helpers;
-using Range = Microsoft.Office.Interop.Word.Range;
+using Xceed.Document.NET;
+using Xceed.Words.NET;
+
 
 namespace Skalk.DocumentGeneration.Service
 {
@@ -14,113 +15,95 @@ namespace Skalk.DocumentGeneration.Service
         {
             byte[] bytes = null;
 
-            // Создание приложения Word
-            Application wordApp = new Application();
-            wordApp.Visible = false;
-
-            // Получение пути к шаблонному файлу
             string projectDirectory = @$"{Directory.GetCurrentDirectory()}\Data\Files";
             string templateFilePath = Path.Combine(projectDirectory, "sample-contract.docx");
+            string pdfFilePath = Path.Combine(projectDirectory, "sample-contract.pdf");
 
-            // Создание временного файла
-            string tempFilePath = Path.GetTempFileName();
-
+            // Создаем временный путь для копии
+            string copiedFilePath = Path.Combine(projectDirectory, "sample-contract-copy.docx");
             try
             {
-                // Копирование шаблона во временный файл
-                File.Copy(templateFilePath, tempFilePath, true);
-
-                // Открытие временной копии документа
-                Document doc = wordApp.Documents.Open(tempFilePath);
-
-                string formattedDate = RuDateAndMoneyConverter.DateToTextLong(newOrderDTO.CreatedAt, "г.");
-
-                string customerInfo = $"{newOrderDTO.CompanyName}, ИНН {newOrderDTO.INN}," +
-                    $" КПП {newOrderDTO.KPP}, {newOrderDTO.PostCode}, {newOrderDTO.Address} ";
-
-                int itemsCount = newOrderDTO?.ItemsOrder?.Count ?? 0;
-                var totalSum = CalculateTotalAmountOfItems(newOrderDTO);
-                var totalSumText = RuDateAndMoneyConverter.CurrencyToTxt(totalSum, true);
-                var nds = CalculateVAT(totalSum);
-                // Нахождение и замена меток на данные
-                ReplaceTagWithText(doc, "<ContractNumber>", newOrderDTO.Id.ToString());
-                ReplaceTagWithText(doc, "<ContractDate>", formattedDate);
-                ReplaceTagWithText(doc, "<InfoCustomer>", customerInfo);
-                ReplaceTagWithText(doc, "<NameCount>", itemsCount.ToString());
-
-                ReplaceTagWithText(doc, "<Itogo>", totalSum.ToString());
-                ReplaceTagWithText(doc, "<NDS>", nds.ToString());
-                ReplaceTagWithText(doc, "<TotalPrice>", totalSum.ToString());
-                ReplaceTagWithText(doc, "<sum>", totalSum.ToString());
-                ReplaceTagWithText(doc, "<sumText>", totalSumText);
-
-                Table table = doc.Tables[5];
-
-                int rowIndex = 1;
-                foreach (var item in newOrderDTO.ItemsOrder)
+                // Копируем файл
+                File.Copy(templateFilePath, copiedFilePath, true);
+                using (DocX doc = DocX.Load(copiedFilePath))
                 {
-                    Row newRow = table.Rows.Add();
+                    string formattedDate = RuDateAndMoneyConverter.DateToTextLong(newOrderDTO.CreatedAt, "г.");
 
-                    newRow.Cells[1].Range.Text = rowIndex.ToString();
-                    newRow.Cells[2].Range.Text = item.Mpn;
-                    newRow.Cells[3].Range.Text = item.Quantity.ToString();
-                    newRow.Cells[4].Range.Text = "шт";
-                    newRow.Cells[5].Range.Text = item.Price.ToString("F2");
-                    newRow.Cells[6].Range.Text = item.TotalAmount.ToString("F2");
+                    string customerInfo = $"{newOrderDTO.CompanyName}, ИНН {newOrderDTO.INN}," +
+                       $" КПП {newOrderDTO.KPP}, {newOrderDTO.PostCode}, {newOrderDTO.Address} ";
 
-                    rowIndex++;
-                }
+                    int itemsCount = newOrderDTO?.ItemsOrder?.Count ?? 0;
+                    var totalSum = CalculateTotalAmountOfItems(newOrderDTO);
+                    var totalSumText = RuDateAndMoneyConverter.CurrencyToTxt(totalSum, true);
+                    var nds = CalculateVAT(totalSum);
+                    // Нахождение и замена меток на данные
+                    ReplaceTagWithText(doc, "<ContractNumber>", newOrderDTO.Id.ToString());
+                    ReplaceTagWithText(doc, "<ContractDate>", formattedDate);
+                    ReplaceTagWithText(doc, "<InfoCustomer>", customerInfo);
+                    ReplaceTagWithText(doc, "<NameCount>", itemsCount.ToString());
 
-                foreach (Row row in table.Rows)
-                {
-                    foreach (Cell cell in row.Cells)
+                    ReplaceTagWithText(doc, "<Itogo>", totalSum.ToString());
+                    ReplaceTagWithText(doc, "<NDS>", nds.ToString());
+                    ReplaceTagWithText(doc, "<TotalPrice>", totalSum.ToString());
+                    ReplaceTagWithText(doc, "<sum>", totalSum.ToString());
+                    ReplaceTagWithText(doc, "<sumText>", totalSumText);
+
+                    Table table = doc.Tables[4];
+
+
+                    int rowIndex = 1;
+                    foreach (var item in newOrderDTO.ItemsOrder)
                     {
-                        cell.Range.Font.Name = "Arial";
-                        cell.Range.Font.Size = 10;
-                        cell.Range.Font.Bold = 0;
+                        Row newRow = table.InsertRow();
+                        // Добавление ячеек в строку и заполнение данными
+                        newRow.Cells[0].Paragraphs.First().Append(rowIndex.ToString());
+                        newRow.Cells[1].Paragraphs.First().Append(item.Mpn);
+                        newRow.Cells[2].Paragraphs.First().Append(item.Quantity.ToString());
+                        newRow.Cells[3].Paragraphs.First().Append("шт");
+                        newRow.Cells[4].Paragraphs.First().Append(item.Price.ToString());
+                        newRow.Cells[5].Paragraphs.First().Append(item.TotalAmount.ToString());
+
+                        rowIndex++;
                     }
+
+                    doc.SaveAs(copiedFilePath);
+
+                    var convertApi = new ConvertApi("2DIawtXjTea2agVg");
+                    var result = await convertApi.ConvertAsync("docx", "pdf", new[]
+                    {
+                        new ConvertApiFileParam(copiedFilePath)
+                    });
+
+                    // Save the PDF file to the same directory
+                    await result.SaveFileAsync(pdfFilePath);
+
+                    // Read the PDF file into a byte array
+                    bytes = await File.ReadAllBytesAsync(pdfFilePath);
+                    return bytes;
                 }
-
-                string pdfFilePath = Path.ChangeExtension(tempFilePath, ".pdf");
-                doc.SaveAs2(pdfFilePath, WdSaveFormat.wdFormatPDF);
-
-
-                doc.Save();
-                doc.Close();
-                // Закрытие приложения Word
-                wordApp.Quit();
-                using (FileStream fileStream = File.OpenRead(pdfFilePath))
-                {
-                    bytes = new byte[fileStream.Length];
-                    await fileStream.ReadAsync(bytes, 0, bytes.Length);
-                }
-
-                // Очистка ресурсов
-                File.Delete(pdfFilePath);
-                Marshal.ReleaseComObject(doc);
-                Marshal.ReleaseComObject(wordApp);
-
             }
-            catch
+            catch(Exception ex)
             {
-                wordApp.Quit();
+                throw new InvalidOperationException(ex.Message);
             }
             finally
             {
-                if (File.Exists(tempFilePath))
+                if (File.Exists(copiedFilePath))
                 {
-                    File.Delete(tempFilePath);
+                    File.Delete(copiedFilePath);
+                }
+
+                if (File.Exists(pdfFilePath))
+                {
+                    File.Delete(pdfFilePath);
                 }
             }
-
-            return bytes;
         }
 
-        private static void ReplaceTagWithText(Document doc, string tag, string text)
+        private void ReplaceTagWithText(DocX document, string tag, string replacement)
         {
-            // Поиск и замена метки на текст в документе
-            Range range = doc.Content;
-            range.Find.Execute(FindText: tag, ReplaceWith: text);
+            // Используем метод ReplaceText для замены меток на данные
+            document.ReplaceText(tag, replacement ?? string.Empty);
         }
 
         private decimal CalculateTotalAmountOfItems(OrderContractDTO order)
